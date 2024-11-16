00934ccc3e8e5d62a0c862e1edcd021f4268d02c
Antonio Jorge Bodoc <ajbodoc@yahoo.com>
2024-11-16 19:02:54 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
-  ],
+],
   providers: [],
   bootstrap: [AppComponent]
 })
